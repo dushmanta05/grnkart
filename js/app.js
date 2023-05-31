a5e1055fc06455ef429637e3eac91d167a77e7fe
Dushmanta <dushmanta0511@gmail.com>
2023-05-31 23:40:01 +0530

    add responsiveness for small device

--- js/app.js
+++ js/app.js
@@ -7,9 +7,21 @@ $(document).ready(function(){
         nextArrow:".next-btn",
         responsive:[
             {
+                breakpoint:1200,
+                settings:{
+                    slidesToShow:2,
+                }
+            },
+            {
                 breakpoint:992,
                 settings:{
                     slidesToShow:2,
+                }
+            },
+            {
+                breakpoint:768,
+                settings:{
+                    slidesToShow:1,
                 }
             }
         ]
